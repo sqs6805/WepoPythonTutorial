0670789576559975160ee7132a99a620909e03ee
shaivit <shaivitomar14@gmail.com>
2023-05-18 21:31:52 -0400

    <List Update>

--- src/Lists.js
+++ src/Lists.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Typography, Grid, CardContent} from '@mui/material'
 
 function Lists(){
     return(
         <div>
-            Lesson 3: Let's learn about Lists! 
+            <Typography color="success">
+                <h1 align="center" color='success'>Lesson 3: Let's learn about Lists!</h1>
+             </Typography>
         </div>
 )
 }

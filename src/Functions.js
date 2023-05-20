07825bf8b21c4e594417985fd768720896b2b1ca
shaivit <shaivitomar14@gmail.com>
2023-05-19 20:40:38 -0400

    <Updates Functions>

--- src/Functions.js
+++ src/Functions.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Functions(){
     return(
         <div>
-            Lesson 5: Let's learn about Functions! 
+            <Typography color="success">
+                <h1 align="center" color='success'>Lesson 5: Let's learn about Functions!</h1>
+             </Typography>
         </div>
 )
 }

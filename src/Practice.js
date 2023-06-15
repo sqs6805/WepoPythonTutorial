2a4274540844f78fff16d74c4be74774c6658119
shaivit <shaivitomar14@gmail.com>
2023-06-15 08:23:45 -0400

    <Practice>

---
+++ src/Practice.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Typography, Grid, Button, CardContent} from '@mui/material'
+import { Card } from '@mui/material'
+
+function Practice(){
+    return(
+        <div>
+            <Typography color="success">
+                <h1 align="center" color='success'>Question 1</h1>
+             </Typography>
+        </div>
+
+)
+}
+export default Practice

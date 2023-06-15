4baac2dd33b7c0306d4f5cdcfe86b9f707f4726b
shaivit <shaivitomar14@gmail.com>
2023-06-14 21:11:26 -0400

    <Trinket>

--- src/Functions.js
+++ src/Functions.js
@@ -11,18 +11,18 @@ function Functions(){
             <Grid container spacing={2}>
                 <Grid item sm ={4}>
             </Grid>
-            <iframe align="center" style={{marginLeft:"40px"}} title='Learning about functions in' src="https://trinket.io/embed/python3/d19782c73e"  width="100" height="356" sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"/>
+            <iframe align="center" style={{marginLeft:"40px"}} title='Learning about functions in' src="https://trinket.io/embed/python3/d19782c73e"  width="400" height="356" sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"/>
             <br></br>
             <br></br>
             <Card style={{marginLeft:"100px", width:"400px", height:"400px",  backgroundColor:"yellowgreen"}} raised="true"></Card>
             <CardContent>
-                    Try yourself!
+                    What are Functions?
                 </CardContent>
                 <CardContent>
-                    Try creating an input yourself now! Ask the user to enter their favorite ice cream flavor. 
+                    When coding, it is common to see a repetition of certain parts of code throughout the program. As a result, it becomes easier to delegate functions to carry out different tasks. So, instead of repeating code, a function can be called to complete the task. In general, functions take an imputed value and then output a value, and are meant to perform specified tasks.
                 </CardContent>
                 <CardContent>
-                    Do this my modifying the sandbox and writing your own code! 
+                    Function Invocation
                 </CardContent>
             </Grid>
         </div>

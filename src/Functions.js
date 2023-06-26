655fe87ce7dfff2412d773694d2845157183ff74
shaivit <shaivitomar14@gmail.com>
2023-06-25 23:08:31 -0400

    <Functions updates>

--- src/Functions.js
+++ src/Functions.js
@@ -9,77 +9,83 @@ function Functions(){
                 <h1 align="center" color='success'>Lesson 5: Let's learn about Functions!</h1>
              </Typography>
             <Grid container spacing={2}>
-                <Grid item sm ={4}>
-            <Grid item sm={4}>
-                    <Card style={{width: "500px", height:"500px", marginLeft:"35px", backgroundColor:"mintcream"}} raised="true">
-                        <CardContent>
-                            <b>What are Functions?</b><b></b>
-                        </CardContent>
-                        <CardContent>
-                            When coding, it is common to see a repetition of certain parts of code throughout the program. As a result, it becomes easier to delegate functions to carry out different tasks. So, instead of repeating code, a function can be called to complete the task. <i>In general, functions take an inputed value and then output a value, and are meant to perform specified tasks.</i>
-                        </CardContent>
-                        <CardContent>
-                            <b>What is Function Invocation?</b><b></b>
-                        </CardContent>
-                        <CardContent>
-                            Functions are invoked by stating the name of the function followed by parentheses which contain the inputs to the function. 
-                            For functions with multiple inputs, separate them with commas.
-                        </CardContent>
-                        <CardContent>
+                <Grid item sm={4}>
+                    <Card style={{marginLeft:"150px", width:"450px", height:"500px", backgroundColor:"yellowgreen"}}>
+                    <CardContent>
+                   <b>What are Functions?</b><b></b>
+                   </CardContent>
+                   <CardContent>
+                   When coding, it is common to see a repetition of certain parts of code throughout the program. <i>In general, functions take an inputed value and then output a value, and are meant to perform specified tasks.</i>
+                   </CardContent>
+                   <CardContent>
+                   <b>What is Function Invocation?</b><b></b>
+                   </CardContent>
+                   <CardContent>
+                   - Functions are invoked by stating the name of the function followed by parentheses which contain the inputs to the function. 
+                   </CardContent>
+                   <CardContent>
+                   - For functions with multiple inputs, separate them with commas.
+                   </CardContent>
+                   <CardContent>
                         <i> For example, a function, called “IceCreamCost” that has one input, ice cream flavor, and one output, the cost, would be invoked like this:
                             IceCreamCost(flavor).</i> 
-                        </CardContent>
-                    </Card>
-                    </Grid>
-                    </Grid>
-                    <br></br>
-                    <br></br>
-                    <Card style={{marginLeft:"200px", width:"610px", height:"600px", backgroundColor:"mintcream"}} raised = "true">
-                        <CardContent>
-                            <b>Defining vs Calling Function</b>
-                        </CardContent>
-                        <CardContent>
-                            Defining a function is done by using the keyword, def following by the function name with the inputs in parenthesis inside.
-                            <i>For example, Def IceCreamCost(flavor) </i> Calling function: IceCreamCost(flavor)
-                        </CardContent>
-                        <CardContent>
-                            Calling a function is done from a separate function and it involves using the function name with the inputs in parenthesis inside. 
-                            <i>For example, IceCreamCost(flavor)</i>
-                        </CardContent>
-                        <CardContent>
-                            <b>Arguments and Parameters</b>
-                        </CardContent>
-                        <CardContent>
-                            An argument is the term for a value that is passed into a function via a function invocation. A parameter is one of the variables in the actual function definition.
-                            For example: IceCreamCost(flavor) -> flavor is an argument and Def IceCreamCost(flavor): -> flavor is a parameter.
-                        </CardContent>
-                        <CardContent>
-                            <b>Return vs Print</b>
-                        </CardContent>
-                        <CardContent>
-                        Return is a keyword that stops the function from continuing and then “returns” the value back to where it was called from. When returning a value, the value will have to be printed inside the function which called it. 
-                            Print is a function the prints out the value for humans to see. It will not have to be printed within the function it was called from. 
-                        </CardContent>
-
-                        <CardContent>
-
-                        </CardContent>
-                    </Card>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <iframe align="center" style={{marginLeft:"40px"}} title='Learning about functions in' src="https://trinket.io/embed/python3/2f76acfe3d"  width="600" height="400" sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"/>
-                    <Card style={{width: "400px", height:"220px", marginLeft:"35px", backgroundColor:"silver"}} raised="true">
-                    <CardContent>
-                        <b>Your turn to try and modify the sandbox!</b>
-                    </CardContent>
-                    <CardContent>
-                        Try and alter the different variables, iceCream_flavors, total_money, cost_per_cone, cones, and see the variety of different outputs!
-                    </CardContent>
-                    <CardContent>
-                        <b> Note how much easier it is to get different outputs and only needing alter a few variables</b>
                     </CardContent>
                 </Card>
+                </Grid>
+                <br></br>
+                <br></br>
+                <Grid item sm={4}>
+                <Card style={{marginLeft:"200px", width:"550px", height:"500px", backgroundColor:"yellowgreen"}}>
+                <CardContent>
+                    <b>Defining vs Calling Function</b>
+                    </CardContent>
+                    <CardContent>
+                        - Defining a function is done by using the keyword, def following by the function name with the inputs in parenthesis inside.
+                    </CardContent>
+                    <CardContent>
+                        - Calling a function is done from a separate function and it involves using the function name with the inputs in parenthesis inside. 
+                    </CardContent>
+                    <CardContent>
+                        <b>Arguments vs Parameters</b>
+                    </CardContent>
+                    <CardContent>
+                        - An argument is the term for a value that is passed into a function via a function invocation. 
+                    </CardContent>
+                    <CardContent>
+                        - A parameter is one of the variables in the actual function definition.
+                    </CardContent>
+                    <CardContent>
+                        <b>Return vs Print</b>
+                    </CardContent>
+                    <CardContent>
+                        - Return is a keyword that stops the function from continuing and then “returns” the value back to where it was called from. 
+                    </CardContent>
+                    <CardContent>
+                        - Print is a function the prints out the value for humans to see. It will not have to be printed within the function it was called from. 
+                    </CardContent>
+                </Card>
+                </Grid>
+                 </Grid>
+                <Grid container spacing={2}>
+                    <Grid item sm={7} align="center">
+                        <br></br>
+                        <br></br>
+                <iframe align="center" style={{marginLeft:"40px"}} title='Learning about functions in' src="https://trinket.io/embed/python3/2f76acfe3d"  width="600" height="400" sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"/>
+                </Grid>
+                <Grid item sm={5}>
+                    <Card style={{marginTop:"75px", width:"300px", height:"300px"}}>
+                <CardContent>
+                <b>Your turn to try and modify the sandbox!</b> Try and alter the different variables and function and see the variety of different outputs!
+                </CardContent>
+  
+                <CardContent>
+                <b> Practice! </b> Now try altering the code by creating a new function that subtracts an amount of ice-cream cones from the store's total supply
+                </CardContent>
+                <CardContent>
+                    <b> Note. This can be done many different ways. Try to experiment and create different variables!</b>
+                </CardContent>
+                    </Card>
+                </Grid>
                 </Grid>
         </div>
 )

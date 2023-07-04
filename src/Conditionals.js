18c142aa09d3544b35fe554daec2c1a2604f54f9
Suvarna <suvarnasaraswat@gmail.com>
2023-07-03 17:28:00 -0700

    finalizing input, conditionals

--- src/Conditionals.js
+++ src/Conditionals.js
@@ -22,7 +22,7 @@ function Conditionals(){
                    <CardContent>
                    Conditionals are used to show different outcomes of different decisions. <b>It has three parts - if statement, elif statement, and else statement.</b>
                    The example given in the sandbox helps us determine the range in which the numerical value was entered by the user. Having an if statement and else statement is the primary 
-                   structure of a conditional with just two conditionals <b>but an elif statement is used when we want to categorize something in more than two categories. </b>
+                   structure of a conditional with just two outcomes <b>but an elif statement is used when we want to categorize something in more than two outcomes. </b>
                    </CardContent>
                 </Typography>
                 </Card>
@@ -36,16 +36,49 @@ function Conditionals(){
                         <b>Sandbox example of conditionals</b>
                     </CardContent>
                 <CardContent>
-                <i>In the example given in the sandbox below, we are separating the input entered by the user into four categories - less than 10, greater than 10 but less than 15, greater than 15 but less than 20 and greater than 20; 
-                and having these different categories produces different results which can be seen by running the code below. The statement that amounts to true is the one that will be executed, and the other statements will be ignored or skipped.  </i>
+                In the example given in the sandbox below, we are separating the input entered by the user into four categories - less than 10, greater than 10 but less than 15, greater than 15 but less than 20 and greater than 20; 
+                and having these different categories produces different results which can be seen by running the code below. The statement that amounts to true is the one that will be executed, and the other statements will be ignored or skipped.
                 </CardContent>
                 </Typography>
                 </Card>
                 </Grid>
-                <Grid item sm={4} align="center">
-                <Card align="center" width="600px" height="600px" raised="true" style={{backgroundColor: "teal", width:"400px", height:"400px", marginRight:"60px"}} >
+                <Card style={{marginTop:"16px", width:"450px", height:"600px", marginLeft:"60px"}}>
+                        <CardContent>
+                            <b>Formatting/Syntax for Conditionals</b>
+                        </CardContent>
+                        <CardContent>
+                            1. Identation is important! The lines of code that you want to execute if an if or elif statement is true, 
+                            should be indented under the declaration of an if statement as shown in the example piece of code. 
+                        </CardContent>
+                        <CardContent>
+                            2. Only the lines of code under the statement that evalutates to true will be executed, the rest of the code will be skipped. 
+                        </CardContent>
+                        <CardContent>
+                            <b>Logical Operators</b>
+                        </CardContent>
+                        <CardContent>
+                            1. ==  : This is an operator to check whether two data points are equal to eachother or not. "print(9==5)" will give us an output of false since 9 and 5 are not equal. However, a "print(5==5)" will give a true result. 
+                        </CardContent>
+                        <CardContent>
+                            2. And : This is an operator which returns true if both the conditions are true. Similar to what AND actually means! For example, in the sandox example, the code within each else if or if statement will only be 
+                            executed if the "and" operators. 
+                        </CardContent>
+                        <CardContent>
+                            3. Or : This is an operator which returns true 
+                        </CardContent>
+                    </Card>
+
+                 </Grid>
+                 <Grid container spacing={2}>
+                    <Grid item sm={7} align="center">
+                        <br></br>
+                        <br></br>
+                <iframe style={{marginLeft:"150px", marginBottom:"50px"}}  align="center" title='Learning about input and output in Python' src="https://trinket.io/embed/python/cbfde46f46"  width="800" height="500" sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"/>
+                </Grid>
+                <Grid item sm={5}>
+                <Card width="600px" height="600px" raised="true" style={{backgroundColor: "teal", width:"400px", height:"400px", marginRight:"50px", marginTop:"100px", marginLeft:"100px"}} >
                 <CardContent>
-                    <Typography color="white" variant="h4" align="center" fontFamily={"GoldmanBold"} >
+                    <Typography color="white" variant="h4" align="center" >
                         <CardContent>Try yourself!</CardContent>
                     </Typography>
                 <Typography color="white" variant='h6' align="left" fontFamily={"GoldmanBold"} marginRight="30px">
@@ -55,27 +88,6 @@ function Conditionals(){
                 </Typography>
                 </CardContent>
                 </Card>
-                </Grid>
-                 </Grid>
-                 <Grid container spacing={2}>
-                    <Grid item sm={7} align="center">
-                        <br></br>
-                        <br></br>
-                <iframe align="center" title='Learning about input and output in Python' src="https://trinket.io/python/d9e7cd51ff"  width="800" height="500" sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"/>
-                </Grid>
-                <Grid item sm={5}>
-                    <Card style={{marginTop:"75px", width:"300px", height:"300px"}}>
-                        <CardContent>
-                            Things to remember!
-                        </CardContent>
-                        <CardContent>
-                            1. Identation is important! The lines of code that you want to execute if an if or elif statement is true, 
-                            should be indented under the declaration of an if statement as shown in the example piece of code. 
-                        </CardContent>
-                        <CardContent>
-                            2. Only the lines of code under the statement that evalutates to true will be executed, the rest of the code will be skipped. 
-                        </CardContent>
-                    </Card>
                 </Grid>
                 </Grid>
         </div>
--- src/Input.js
+++ src/Input.js
@@ -10,12 +10,70 @@ function Input(){
              </Typography>
             <Grid container spacing={2}>
                 <Grid item sm ={4}>
-            <iframe align="center" style={{marginLeft:"40px"}} title='Learning about input and output in Python' src="https://trinket.io/embed/python3/860e806044"  width="600" height="400" sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"/>
+
+            <Card style={{backgroundColor:"yellowgreen", width:"400px", height:"600px", marginRight:"80px", marginLeft:"50px", marginTop:"30px"}} raised="true">
+            <CardContent>
+                <b>Variables</b>
+            </CardContent>
+            <CardContent>
+                Variables is a way to store data. You can assign something to a variable when you want to use it in your code somewhere. 
+            </CardContent>
+            <CardContent>
+                For example, if you see the example given below in the sandbox, "favorite_car_brand" is actually a variable which is storing the data point that the user is entering.
+            </CardContent>
+            <CardContent>
+                Variables is a simple concept but there are certain rules that you need to keep in mind while declaring these: 
+            </CardContent>
+            <CardContent>
+            1. A variable name must start with a letter or the underscore character
+            </CardContent>
+            <CardContent>
+            2. A variable name cannot start with a number
+            </CardContent>
+            <CardContent>
+            3. A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+            </CardContent>
+            <CardContent>
+            4. Variable names are case-sensitive (age, Age and AGE are three different variables)
+            </CardContent>
+            </Card>
             <br></br>
             <br></br>
-            <Card style={{marginLeft:"100px", width:"400px", height:"400px",  backgroundColor:"yellowgreen"}} raised="true">
+            <iframe align="center" style={{marginLeft:"300px", marginBottom:"15px"}} title='Learning about input and output in Python' src="https://trinket.io/embed/python/024ee9fc0a"  width="600" height="400" sandbox="allow-forms allow-modals allow-presentation allow-same-origin allow-scripts"/>
+            
+            </Grid>
+            <Grid item sm={4}>
+            <Card style={{backgroundColor:"yellowgreen", width:"480px", height:"600px", marginRight:"60px", marginTop:"30px"}} raised="true">
+                    <CardContent>
+                        <Typography>
+                            <b>Data Types</b>
+                        </Typography>
+                    </CardContent>
+                    <CardContent>
+                        The data stored in variables can be of different types, for example, if it is number it is either an int or float, if it is something in quotes then it is a string. More data types are given below - 
+                    </CardContent>
+                    <Typography>
+                        <CardContent>
+                            1. Integer - Non decimal numerical values are categorized as "int". For example, 1, 2, 3.. 
+                        </CardContent>
+                        <CardContent>
+                            2. Float - These are the numeric decimal values. For example, 2.3, 7.8, 4.0
+                        </CardContent>
+                        <CardContent>
+                            3. String - Anything having " " around it can be categorized as a string value. For example, "name", "apple", "4"
+                        </CardContent>
+                        <CardContent>
+                            4. Boolean - This can have only two values - true of false. For example, if an object has a property that is present, the value of the property will be <b>true</b>, otherwise it will be <b>false</b>
+                        </CardContent>
+                        <CardContent>
+                            In the sandbox below try doing "print(type("5"))". Type is a function that helps us determine the 
+                            type of variable. 
+                        </CardContent>
+                    </Typography>
+                </Card>
+                <Card style={{marginLeft:"400px", width:"400px", height:"200px", marginTop:"100px",  backgroundColor:"yellowgreen"}} raised="true">
                 <CardContent>
-                    Try yourself!
+                    <b>Try yourself!</b>
                 </CardContent>
                 <CardContent>
                     Try creating an input yourself now! Ask the user to enter their favorite ice cream flavor. 
@@ -26,7 +84,7 @@ function Input(){
             </Card>
             </Grid>
             <Grid item sm={4}>
-                <Card style={{width: "400px", height:"400px", marginLeft:"120px", backgroundColor:"yellowgreen"}} raised="true">
+            <Card style={{width: "400px", height:"600px", marginLeft:"5px", marginTop:"30px", backgroundColor:"yellowgreen"}} raised="true">
                     <CardContent>
                         <b>Input/Output</b>
                     </CardContent>
@@ -42,50 +100,18 @@ function Input(){
                         <b>The way to get an output from the code is using print statements. </b> The print function takes in a variable that 
                         is supposed to be printed and shown to the user, which in other words means giving an output. 
                     </CardContent>
-                </Card>
-            </Grid>
-            <Grid item sm={4}>
-                <Card style={{backgroundColor:"yellowgreen", width:"450px", height:"550px", marginRight:"60px"}} raised="true">
-                    <CardContent>
-                        <Typography>
-                            <b>Different types of data types:</b>
-                        </Typography>
-                    </CardContent>
-                    <Typography>
-                        <CardContent>
-                            1. Integer - Non decimal numerical values are categorized as "int". For example, 1, 2, 3.. 
-                        </CardContent>
-                        <CardContent>
-                            2. Float - These are the numeric decimal values. For example, 2.3, 7.8, 4.0
-                        </CardContent>
-                        <CardContent>
-                            3. String - Anything having " " around it can be categorized as a string value. For example, "name", "apple", "4"
-                        </CardContent>
-                        <CardContent>
-                            4. Boolean - This can have only two values - true of false. Used as a check for different properties. For example, if an object has a property that is present, the value of the property will be <b>true</b>, otherwise it will be <b>false</b>
-                        </CardContent>
-                        <CardContent>
-                            Go to the sandbox and try printing "type(5)", type("5"). Type is a function that helps us determine the 
-                            type of a given variable. 
-                        </CardContent>
-                    </Typography>
-                </Card>
-                <br></br>
-                <br></br>
-                <br></br>
-                <Card style={{backgroundColor:"yellowgreen", width:"450px", height:"200px", marginRight:"60px"}} raised="true">
-                    <Typography style={{color:"black"}}>
                     <CardContent>
                         <b>Comments</b>
                     </CardContent>
                     <CardContent>
-                        This is a way to write a desciption of your code that won't affect the execution of the program. 
+                        This is a way to write a desciption of your code that won't affect the execution of the program.  A line is considered as a comment in Python if it starts with a '#' symbol. 
                     </CardContent>
-                    <CardContent>
-                        A line is considered as a comment in Python if it starts with a '#' symbol. 
-                    </CardContent>
-                    </Typography>
                 </Card>
+
+                
+                <br></br>
+                <br></br>
+                <br></br>
                 </Grid>
             </Grid>
         </div>

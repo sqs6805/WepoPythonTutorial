a5e22fce4c9c07b044ada3c320a22e00828d3797
Suvarna <suvarnasaraswat@gmail.com>
2023-07-08 07:35:26 -0700

    deploying website

---
+++ build/precache-manifest.c9df947371556d0e0c2f0af46c3e325b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229647d1f13885dcd801",
+    "url": "/static/css/main.ea6dd171.chunk.css"
+  },
+  {
+    "revision": "229647d1f13885dcd801",
+    "url": "/static/js/main.229647d1.chunk.js"
+  },
+  {
+    "revision": "71cb52921b291a00cde5",
+    "url": "/static/js/1.71cb5292.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dc1521f9ac81c60ecac2e8b18f663d10",
+    "url": "/index.html"
+  }
+];

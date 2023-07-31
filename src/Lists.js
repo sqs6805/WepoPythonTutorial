b4746777a6efc19f4ed7148ad3378f35f14e2130
avaestephens <133178348+avaestephens@users.noreply.github.com>
2023-07-30 22:44:37 -0400

    <Lists and Loops edits>

--- src/Lists.js
+++ src/Lists.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography, Grid, CardContent} from '@mui/material'
+import { Typography, Grid, Button, CardContent} from '@mui/material'
 import { Card } from '@mui/material'
 
 function Lists(){
@@ -10,7 +10,7 @@ function Lists(){
              </Typography>
             <Grid container spacing={2}>
                 <Grid item sm ={4}>
-                <Card style={{marginLeft:"100px", width:"700px", height:"325px", backgroundColor:"yellowgreen"}} raised="true">
+                <Card style={{marginLeft:"100px", width:"700px", height:"375px", backgroundColor:"white"}} raised="true">
                 <CardContent>
                     <Typography>
                         <b>What are Lists and why are they used?</b>
@@ -21,7 +21,7 @@ function Lists(){
                         A list is a <b>mutable</b> (can be changed) data structure in Python. A list consists of an ordered <b>sequence of elements</b>, each of these elements inside the list is called an <b>item</b>. All python lists are framed by an outerset of <b>square brackets [ ]</b>. Any data types can be added to a single list.
                         </CardContent>
                         <CardContent>
-                            Lists can become useful when working with <b>related values and data sets</b>. Lists are an easy way to keep data organized and perform the same operations on multiple values at once. These operations can be performed using different <b>list functions</b>. Examples of these functions are given on the next card.  
+                            Lists can become useful when working with <b>related values and data sets</b>. Lists are an easy way to keep data organized and perform the same operations on multiple values at once. These operations can be performed using different <b>list functions</b>. Examples of these functions are given on the next card. To retrieve an item from a list, use the list name followed by square brackets framing the index of the item you want to access. Ex: list_name[0] - retrieves the first element of a list. 
                         </CardContent>
                     </Typography>
             </Card>
@@ -35,33 +35,30 @@ function Lists(){
             <Grid item sm={4}>
             </Grid>
             <Grid item sm={4}>
-                <Card style={{backgroundColor:"yellowgreen", width:"400px", height:"700px", marginRight:"100px"}} raised="true">
+                <Card style={{backgroundColor:"white", width:"400px", height:"650px", marginRight:"100px"}} raised="true">
                     <CardContent>
                         <Typography>
-                            <b>Important Python List Functions</b>
+                            <b>Important Python List Functions:</b>
                         </Typography>
                     </CardContent>
                     <Typography>
                         <CardContent>
-                            <b>1. len()</b> - Returns the number of items in the List.
+                            <b>1. len(list_name)</b> - Returns the number of items in the List.
                         </CardContent>
                         <CardContent>
-                            <b>2. append()</b> - Used for appending and adding elements to the end of the List.
+                            <b>2. append(element)</b> - Used for appending and adding elements to the end of the List.
                         </CardContent>
                         <CardContent>
-                            <b>3. remove()</b> - Removes a given object from the List. 
+                            <b>3. remove(element)</b> - Removes a given object from the List. 
                         </CardContent>
                         <CardContent>
                             <b>4. sort()</b> - Sort a List in ascending, descending, or user-defined order.
                         </CardContent>
                         <CardContent>
-                            <b>5. count()</b> - Returns the number of times an object appears in a List.
+                            <b>5. count(element)</b> - Returns the number of times an element appears in a List.
                         </CardContent>
                         <CardContent>
-                            <b>6. min() / max()</b> - Calculates the minimum or maximum of all the elements of the List
-                        </CardContent>
-                        <CardContent>
-                            <b>7. sort()</b> - Sort a List in ascending, descending, or user-defined order.
+                            <b>6. min(list) / max(list)</b> - Calculates the minimum or maximum of all the elements of the List
                         </CardContent>
                         <CardContent>
                              Run the code in the sandbox and to see how ice cream flavors can be added to a list that acts as the shopping cart. 
@@ -71,10 +68,13 @@ function Lists(){
                 <br></br>
                 <br></br>
                 <br></br>
-                <Card style={{backgroundColor:"yellowgreen", width:"400px", height:"100px", marginRight:"100px"}} raised="true">
+                <Card style={{backgroundColor:"white", width:"400px", height:"150px", marginRight:"100px"}} raised="true">
                     <Typography style={{color:"black"}}>
                     <CardContent>
-                        <b>Your turn to modify the sandbox!</b> - Now try using the <b>costs list</b> to add up the total cost of the shopping cart.
+                        <b>Your turn to modify the sandbox!</b> 
+                    </CardContent>
+                    <CardContent>
+                        Now try using the <b>costs list</b> to add up the total cost of the shopping cart.
                     </CardContent>
                     </Typography>
                 </Card>
--- src/Loops.js
+++ src/Loops.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography, Grid, CardContent} from '@mui/material'
+import { Typography, Grid, Button, CardContent} from '@mui/material'
 import { Card } from '@mui/material'
 
 function Loops(){
@@ -10,7 +10,7 @@ function Loops(){
              </Typography>
             <Grid container spacing={2}>
                 <Grid item sm ={4}>
-                <Card style={{marginLeft:"100px", width:"700px", height:"275px", backgroundColor:"yellowgreen"}} raised="true">
+                <Card style={{marginLeft:"100px", width:"700px", height:"350px", backgroundColor:"white"}} raised="true">
                 <CardContent>
                     <Typography>
                         <b>What are Loops and why are they used?</b>
@@ -23,6 +23,9 @@ function Loops(){
                         <CardContent>
                             Loops are important because they allow a programmer to execute blocks of code repeatedly without having to write out the same line of code multiple times. An advantage of using loops is that it <b>increases efficiency</b> and allows code to become <b>more simplified</b>.
                         </CardContent>
+                        <CardContent>
+                            A <b>":"</b> must be placed after the first line of each loop. This indicates the sart of an indented block of code.
+                        </CardContent>
                     </Typography>
             </Card>
 
@@ -31,11 +34,12 @@ function Loops(){
 
             <iframe align="center" style={{marginLeft:"100px"}} title='Learning about Loops in Python' src= "https://trinket.io/embed/python/15078468c5?toggleCode=true"  width="700" height="500" sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"/>
 
+
             </Grid>
             <Grid item sm={4}>
             </Grid>
             <Grid item sm={4}>
-                <Card style={{backgroundColor:"yellowgreen", width:"400px", height:"600px", marginRight:"50px"}} raised="true">
+                <Card style={{backgroundColor:"white", width:"400px", height:"585px", marginRight:"50px"}} raised="true">
                     <CardContent>
                         <Typography>
                             <b>Different Types of Loops</b>
@@ -59,10 +63,13 @@ function Loops(){
                 <br></br>
                 <br></br>
                 <br></br>
-                <Card style={{backgroundColor:"yellowgreen", width:"400px", height:"110px", marginRight:"100px"}} raised="true">
+                <Card style={{backgroundColor:"white", width:"400px", height:"225px", marginRight:"100px"}} raised="true">
                     <Typography style={{color:"black"}}>
                     <CardContent>
                         <b>Your turn to modify the sandbox!</b> - Print out all the ice cream flavors in the list using a <b>for loop</b>. The while loop was already done for you.
+                    </CardContent>
+                    <CardContent>
+                        <b>Notes: len()</b> is a list function we saw in the last lesson, feel free to go back and review it if you are confused! <b>"i"</b> is a variable place holder used to track the amount of times the loop is traversed.
                     </CardContent>
                     </Typography>
                 </Card>
